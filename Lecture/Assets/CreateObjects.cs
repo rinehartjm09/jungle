d184322e202894c2a3668f2c3dba1ab433916580
Joe Rinehart <rinehartjm09@gmail.com>
2017-05-17 19:20:57 -0700

    First push of project

---
+++ Lecture/Assets/CreateObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObjects : MonoBehaviour {
+	public GameObject objectToCreate;
+	public float startXMin, startXMax;
+	public float startYMin, startYMax;
+	public int numberOfObjects;
+	// Use this for initialization
+	void Start () {
+		CreateRandomObject ();
+	}
+
+	void CreateRandomObject(){
+		for (int i = 0; i < numberOfObjects; i++) {
+			float x = Random.Range (startXMin, startXMax);
+			float y = Random.Range (startYMin, startYMax);
+			Instantiate (objectToCreate, new Vector3 (x, y, 0), Quaternion.identity);
+		}
+	}
+}
---
+++ Lecture/Assets/MovePlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+	public float playerSpeed = 1.0f;
+	public float playerJump = 10.0f;
+	private bool onGround = true;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+		
+	// Update is called once per frame
+	void FixedUpdate () {
+//				onGround = true;
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate (-playerSpeed * Time.smoothDeltaTime, 0, 0);
+	}
+		if (Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate (playerSpeed * Time.smoothDeltaTime, 0, 0);
+		}
+//		if (Input.GetKey(KeyCode.UpArrow)){
+//			transform.Translate (0, playerSpeed * Time.smoothDeltaTime, 0);
+//		}
+//		if (Input.GetKey(KeyCode.DownArrow)){
+//			transform.Translate (0, -playerSpeed * Time.smoothDeltaTime, 0);
+//		}
+		Rigidbody2D player = GetComponent<Rigidbody2D> ();
+		if (Input.GetKeyDown(KeyCode.Space) && onGround){
+			player.AddForce (Vector2.up * playerJump);
+//			onGround = false; 
+		}
+
+}
+}
